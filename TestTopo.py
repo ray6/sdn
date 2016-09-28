0a2b5675278d4167265169983d071fc0aa3a3129
ray6 <ray6.ray61027@gmail.com>
2016-09-28 12:11:11 +0800

    The example topology for testing

---
+++ TestTopo.py
@@ -0,0 +1,39 @@
+'''
+SDN project testing topo
+       s1
+      /  \
+     s2  s3
+     |    |
+   host.. host...
+
+'''
+from mininet.topo import Topo
+
+class TestTopo( Topo ):
+	
+	def __init__( self , n=2 ):
+
+		# Initialize topology
+		Topo.__init__( self)
+
+		# Add Host
+		h1 = self.addHost( 'h1' )
+		h2 = self.addHost( 'h2' )
+		h3 = self.addHost( 'h3' )
+		h4 = self.addHost( 'h4' )
+		
+		# Add Switch
+		s1 = self.addSwitch( 's1' )
+		s2 = self.addSwitch( 's2' )
+		s3 = self.addSwitch( 's3' )
+
+		# Add Link
+		self.addLink( s1, s2 )
+		self.addLink( s1, s3 )
+		self.addLink( s2, h1 )
+		self.addLink( s2, h2 )
+		self.addLink( s3, h3 )
+		self.addLink( s3, h4 )
+
+topos = { 'TestTopo': ( lambda: TestTopo() ) }
+         
