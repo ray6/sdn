bba1cf4822be72d07574429654ebab0063ae7307
tzuhan1106 <smile110611@gmail.com>
2017-01-05 01:21:23 +0800

    merge

--- TestTopo.py
+++ TestTopo.py
@@ -21,7 +21,8 @@ class TestTopo( Topo ):
 		h2 = self.addHost( 'h2' )
 		h3 = self.addHost( 'h3' )
 		h4 = self.addHost( 'h4' )
-		
+                h5 = self.addHost( 'h5' )
+		h6 = self.addHost( 'h6' )		
 		# Add Switch
 		s1 = self.addSwitch( 's1' )
 		s2 = self.addSwitch( 's2' )
@@ -30,10 +31,12 @@ class TestTopo( Topo ):
 		# Add Link
 		self.addLink( s1, s2 )
 		self.addLink( s1, s3 )
+		self.addLink( s2, s3 )
 		self.addLink( s2, h1 )
 		self.addLink( s2, h2 )
+                self.addLink( s1, h5 )
 		self.addLink( s3, h3 )
 		self.addLink( s3, h4 )
-
+		self.addLink( s1, h6 )
 topos = { 'TestTopo': ( lambda: TestTopo() ) }
          
